ce39bc3598863188cb7992cfca50a6c1c28f4b32
Vladimir Manushko <vladimir.manushko@gmail.com>
2017-01-31 22:13:58 +0200

    short_url field was added to the shortener app's model

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class UrlShortener(models.Model):
 
     url = models.CharField(max_length=255)
+    short_url = models.CharField(max_length=15, unique=True)
 
     def __str__(self):
         return self.url
