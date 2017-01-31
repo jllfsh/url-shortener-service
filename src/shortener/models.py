b04b6894cb2a55d14b22ab52595fab81d1a69387
Vladimir Manushko <vladimir.manushko@gmail.com>
2017-01-31 22:23:46 +0200

    created and updated fields were added to the shortener app's model

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -3,8 +3,10 @@ from django.db import models
 
 class UrlShortener(models.Model):
 
-    url = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
     short_url = models.CharField(max_length=15, unique=True)
+    updated = models.DateTimeField(auto_now=True)
+    url = models.CharField(max_length=255)
 
     def __str__(self):
         return self.url
