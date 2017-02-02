844a1a01931e5f256aec3f6243b1d4d4c11177f4
Vladimir Manushko <vladimir.manushko@gmail.com>
2017-02-02 22:36:03 +0200

    detail view was changed to redirect view

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from shortener.views import UrlShortenerDetailView
+from shortener.views import UrlShortenerRedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^(?P<short_url>[-\w]+)', UrlShortenerDetailView.as_view())
+    url(r'^(?P<short_url>[-\w]+)', UrlShortenerRedirectView.as_view())
 ]
--- src/shortener/views.py
+++ src/shortener/views.py
@@ -1,13 +1,13 @@
-from django.http.response import HttpResponse
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.views.generic import View
 
 from .models import UrlShortener
 
 
-class UrlShortenerDetailView(View):
-    """Shows full URL for short URL."""
+class UrlShortenerRedirectView(View):
+    """Redirects to full URL for certain short URL."""
     @staticmethod
     def get(request, short_url, *args, **kwargs):
         obj = get_object_or_404(UrlShortener, short_url=short_url)
-        return HttpResponse('URL: {url}'.format(url=obj.url))
+        return HttpResponseRedirect(obj.url)
