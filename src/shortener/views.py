e76fb17614e4fac1a995d5eb988b87e40d041337
Vladimir Manushko <vladimir.manushko@gmail.com>
2017-02-02 22:27:43 +0200

    detail view to show full url for short url was made

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.views.generic import View
 
-# Create your views here.
+from .models import UrlShortener
+
+
+class UrlShortenerDetailView(View):
+    """Shows full URL for short URL."""
+    @staticmethod
+    def get(request, short_url, *args, **kwargs):
+        obj = get_object_or_404(UrlShortener, short_url=short_url)
+        return HttpResponse('URL: {url}'.format(url=obj.url))
