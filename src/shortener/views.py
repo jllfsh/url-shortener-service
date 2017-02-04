2d8c67b4db70adea156e171f114900db261eeef5
Vladimir Manushko <vladimir.manushko@gmail.com>
2017-02-04 22:06:44 +0200

    shortener's app views were improved to generic views

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -1,27 +1,26 @@
-from django.http.response import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from django.views.generic import CreateView, View
+from django.views.generic import CreateView, RedirectView
 
-from .forms import CreateURLFrom
 from .models import UrlShortener
 
 
 class HomeView(CreateView):
     """Home page view."""
     template_name = 'shortener/home.html'
-    form_class = CreateURLFrom
+    model = UrlShortener
+    fields = ('url',)
 
     def form_valid(self, form):
         super().form_valid(form)
-        return render(self.request, self.template_name, context={
+        context = {
             'form': form,
             'short_url': self.request.build_absolute_uri(self.object.get_absolute_url())
-        })
+        }
+        return render(self.request, self.template_name, context)
 
 
-class UrlShortenerRedirectView(View):
+class UrlShortenerRedirectView(RedirectView):
     """Redirects to full URL for certain short URL."""
-    @staticmethod
-    def get(request, short_url, *args, **kwargs):
-        obj = get_object_or_404(UrlShortener, short_url=short_url)
-        return HttpResponseRedirect(obj.url)
+    def get_redirect_url(self, short_url, *args, **kwargs):
+        url_object = get_object_or_404(UrlShortener, short_url=short_url)
+        return url_object.url
